9c1e143db2fefecb1c3d832ebe32e71eaeb3d252
ilijavishinov-pc <ilijavishinovprofessional@gmail.com>
2023-10-22 12:12:07 +0200

    separate documentation database in new class DocumentationHandler

--- utils_dir/documentation_agent.py
+++ utils_dir/documentation_agent.py
@@ -1,20 +1,4 @@
-from langchain.docstore.document import Document
-import torch
-import tqdm
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.embeddings import HuggingFaceEmbeddings
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, pipeline, AutoModel, RobertaForCausalLM, AutoModelForQuestionAnswering
-from langchain.vectorstores import Chroma
-from langchain.embeddings import LlamaCppEmbeddings, GPT4AllEmbeddings
-from pathlib import Path
-from langchain.document_loaders import (
-    TextLoader, UnstructuredMarkdownLoader
-)
 import os
-import utils_dir.text_processing as text_processing
-from langchain.text_splitter import RecursiveCharacterTextSplitter, MarkdownTextSplitter, MarkdownHeaderTextSplitter, CharacterTextSplitter, SentenceTransformersTokenTextSplitter
-from utils_dir_backup.ingest_data import sentence_to_vector
-
 from utils_dir.documentation_handler import DocumentationHandler
 from utils_dir.llm_agent import LlmAgent
 from utils_dir.qa_agent import QaAgent
@@ -25,83 +9,45 @@ os.environ["OPENAI_API_KEY"] = "sk-fNE2GMef6ITw79K7EhraT3BlbkFJB7Kw3PBtrMzJklCts
 
 class DocumentationAgent:
     
-    documents = []
-    texts = []
-    docs_dir = None
-    db = None
-    
+    documentation_handler = None
     embedding_agent = None
     qa_agent = None
     llm_agent = None
     
-    
     def __init__(self,
                  db_dir = None,
-                 standalone_chroma_db = False,
-                 embedding_model_name = 'distilbert',
-                 llm_model_name = None,
-                 qa_model_name = None):
+                 embedding_model_name = None,
+                 answering_model_name = None):
         
-        self.db_dir = db_dir
-        self.standalone_chroma_db = standalone_chroma_db
+        self.documentation_handler = DocumentationHandler(db_dir = db_dir)
+        self.embedding_agent = DocumentationEmbedder(embedding_model_name = embedding_model_name)
         
-        self.embedding_agent = DocumentationEmbedder(embedding_model_name)
-
-        if qa_model_name:
-            self.qa_agent = QaAgent(qa_model_name)
-
-        if llm_model_name:
-            self.llm_agent = LlmAgent(llm_model_name)
+        if answering_model_name.startswith("qa_"):
+            self.qa_agent = QaAgent(qa_model_name = answering_model_name)
+        else:
+            self.llm_agent = LlmAgent(llm_model_name = answering_model_name)
     
     def load_documentation_folder(self,
                                   docs_dir,
                                   text_splitter_name,
-                                  chunk_size = None,
+                                  chunk_size,
                                   similarity_metric_name = 'cosine'):
         """
         """
         
-        # load the embedding model
-        self.get_embeddings_object()
+        self.documentation_handler.load_documentation_folder(embedding_agent = self.embedding_agent,
+                                                             docs_dir = docs_dir,
+                                                             text_splitter_name = text_splitter_name,
+                                                             chunk_size = chunk_size,
+                                                             similarity_metric_name = similarity_metric_name)
+    
+    def get_response(self,
+                     query):
+        """
+        """
         
-        # assertions
-        if text_splitter_name == 'tokens' and self.embedding_model_name not in ['distilbert', 'roberta', 'bert']:
-            raise NameError(f"tokens chunking implementation for {self.embedding_model_name} is not supported")
-        
-        # define the db folder name based on chunking and embedding parameters
-        dir_suffix = f"{self.embedding_model_name}_{similarity_metric_name}_{text_splitter_name}"
-        if chunk_size:
-            dir_suffix += f'_{"-".join([str(i) for i in chunk_size])}'
-        persist_directory = os.path.join(self.db_dir, f'db_{os.path.basename(os.path.normpath(docs_dir))}_{dir_suffix}')
-        persist_directory_standalone = os.path.join(self.db_dir, f'db_{os.path.basename(os.path.normpath(docs_dir))}_{dir_suffix}_standalone')
-        
-        # load chroma db, or create if it does not exist
-        if not os.path.exists(persist_directory):
-            self.read_documents(docs_dir)
-            self.split_documents(text_splitter_name = text_splitter_name, chunk_size = chunk_size)
-            chroma = Chroma.from_documents(self.texts,
-                                           self.embedding_model,
-                                           persist_directory = persist_directory,
-                                           collection_metadata = {"hnsw:space": similarity_metric_name})
+        if self.llm_agent:
+            return self.llm_agent.llm_rag(query, self.documentation_handler.db)
         else:
-            chroma = Chroma(persist_directory = persist_directory,
-                            embedding_function = self.embedding_model,
-                            collection_metadata = {"hnsw:space": similarity_metric_name})
-        
-        self.db = chroma
-    
-    def llm_rag(self,
-                query):
-        """
-
-        """
-        return self.llm_agent.llm_rag(query, self.db)
-    
-    def qa_response(self,
-                    query):
-        """
-        
-        """
-        
-        return self.qa_agent.qa_response(query, self.db)
-    
+            return self.qa_agent.qa_response(query, self.documentation_handler.db)
+            
---
+++ utils_dir/documentation_handler.py
@@ -0,0 +1,136 @@
+import torch
+import tqdm
+from langchain.chat_models import ChatOpenAI
+from langchain.llms import OpenAI
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.embeddings import HuggingFaceEmbeddings
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, pipeline, AutoModel, RobertaForCausalLM, AutoModelForQuestionAnswering
+from langchain import HuggingFacePipeline
+from langchain.llms import HuggingFacePipeline
+from langchain.llms import LlamaCpp, GPT4All
+from langchain.chains import RetrievalQA
+from langchain.vectorstores import Chroma
+from langchain.embeddings import LlamaCppEmbeddings, GPT4AllEmbeddings
+from pathlib import Path
+from langchain.document_loaders import (
+    TextLoader, UnstructuredMarkdownLoader
+)
+import os
+import utils_dir.text_processing as text_processing
+from langchain.text_splitter import RecursiveCharacterTextSplitter, MarkdownTextSplitter, MarkdownHeaderTextSplitter, CharacterTextSplitter, SentenceTransformersTokenTextSplitter
+from langchain.prompts import PromptTemplate
+
+os.environ["OPENAI_API_KEY"] = "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"
+
+
+class DocumentationHandler:
+    documents = []
+    texts = []
+    docs_dir = None
+    db = None
+    
+    def __init__(self,
+                 db_dir = None):
+        
+        self.db_dir = db_dir
+    
+    def read_documents(self,
+                       docs_dir):
+        """
+        """
+        
+        glob = Path(f"{docs_dir}").glob
+        ps = list(glob("**/*.md"))
+        documents = list()
+        for p in tqdm.tqdm(ps, "Loading documents"):
+            file_extension = os.path.splitext(p)[1]
+            if file_extension != '.md': continue
+            document = UnstructuredMarkdownLoader(p, encoding = "utf-8").load()[0]
+            document.page_content = text_processing.markdown_to_lower_text(document.page_content)
+            document.metadata["source"] = document.metadata['source'].__str__()
+            documents.append(document)
+        self.documents = documents
+    
+    def split_documents(self,
+                        embedding_agent,
+                        chunk_size,
+                        text_splitter_name = 'recursive'):
+        """
+
+        """
+        
+        text_splitter = None
+        
+        if type(chunk_size) is int:
+            chunk_size = [chunk_size]
+        
+        chunk_sizes = chunk_size
+        
+        for chunk_size in chunk_sizes:
+            chunk_overlap = chunk_size // 3
+            
+            if text_splitter_name == 'recursive':
+                text_splitter = RecursiveCharacterTextSplitter(
+                    chunk_size = chunk_size,
+                    chunk_overlap = chunk_overlap,
+                    separator = ' '
+                )
+            elif text_splitter_name == 'character':
+                text_splitter = CharacterTextSplitter(
+                    chunk_size = chunk_size,
+                    chunk_overlap = chunk_overlap,
+                    separator = ' '
+                )
+            elif text_splitter_name == 'markdown':
+                text_splitter = MarkdownTextSplitter()
+            elif text_splitter_name == 'tokens':
+                text_splitter = CharacterTextSplitter.from_huggingface_tokenizer(
+                    embedding_agent.embedding_tokenizer,
+                    chunk_size = chunk_size,
+                    chunk_overlap = chunk_overlap,
+                    separator = ' '
+                )
+            
+            if not text_splitter:
+                raise NameError("The text_splitter_name that you entered is not supported")
+            
+            texts = text_splitter.split_documents(self.documents)
+            self.texts.extend(texts)
+            
+    
+    def load_documentation_folder(self,
+                                  embedding_agent,
+                                  docs_dir,
+                                  text_splitter_name,
+                                  chunk_size = None,
+                                  similarity_metric_name = 'cosine'):
+        """
+        """
+        
+        # assertions
+        if text_splitter_name == 'tokens' and embedding_agent.embedding_model_name not in ['distilbert', 'roberta', 'bert']:
+            raise NameError(f"tokens chunking implementation for {embedding_agent.embedding_model_name} is not supported")
+        
+        # define the db folder name based on chunking and embedding parameters
+        dir_suffix = f"{embedding_agent.embedding_model_name}_{similarity_metric_name}_{text_splitter_name}"
+        if chunk_size:
+            dir_suffix += f'_{"-".join([str(i) for i in chunk_size])}'
+        persist_directory = os.path.join(self.db_dir, f'db_{os.path.basename(os.path.normpath(docs_dir))}_{dir_suffix}')
+        
+        # load chroma db, or create if it does not exist
+        if not os.path.exists(persist_directory):
+            self.read_documents(docs_dir)
+            self.split_documents(embedding_agent = embedding_agent,
+                                 text_splitter_name = text_splitter_name,
+                                 chunk_size = chunk_size)
+            chroma = Chroma.from_documents(self.texts,
+                                           embedding_agent.embedding_model,
+                                           persist_directory = persist_directory,
+                                           collection_metadata = {"hnsw:space": similarity_metric_name})
+        else:
+            chroma = Chroma(persist_directory = persist_directory,
+                            embedding_function = embedding_agent.embedding_model,
+                            collection_metadata = {"hnsw:space": similarity_metric_name})
+        
+        self.db = chroma
+    
